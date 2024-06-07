12eaaf2070b437a389ff293d9c393e77ba3281c7
misterj0cker <misteryassire@gmail.com>
2024-06-07 13:54:00 +0100

    Add files via upload

--- t.c
+++ t.c
@@ -39,3 +39,65 @@ printf(",");
 affiche_arbre_rec(arbre−>droit);
 }
 }
+
+
+
+
+
+struct Element{
+    int val;
+    struct Element *suivant;
+};
+typedef struct Element LISTE;
+
+LISTE * elem_new(int val){
+LISTE *new_elem;
+new_elem = (LISTE*)malloc(sizeof(LISTE));
+new_elem->val = val;
+new_elem->suivant = NULL
+return new_elem;
+}
+
+void * elem_free(LISTE *elem)
+{
+    free(elem);
+}
+
+int list_length(LSITE *L){
+    int len=0
+    while(L!=NULL)
+    {
+        L = L->suivant; len++;
+    }
+    return len
+}
+
+void list_print(LISTE *L){
+    int i;
+    for(i=0 ; L; num++,L=L->suivant)
+    printf("Element %d = %d \n",num,L->val);
+}
+
+LISTE * list_push(LISTE *L,LISTE *elem){
+    elem->suivant = L;
+    return elem;
+}
+
+void liste_insert(Liste *P,LISTE *elem){
+    elem->suivant = P->suivant;
+    P->suivant = elem
+    LISTE *L = NULL,*P;
+    P = L->suivant;
+    list_insert(P,elem);
+}
+
+void liste_append(LISTE *L,LISTE *elem){
+    LISTE *cur;
+    if(L==NULL)
+    return list_push(L,elem);
+    cur =L;
+    while(cur->suivant != NULL)
+    cur = cur->suivant;
+    liste_insert(cur,elem);
+    return L
+}
